189d2bac39920653bc23dc44ba74515541b7a949
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-11-12 18:07:48 -0300

    commit 3

---
+++ js/metodoFilter.js
@@ -0,0 +1,16 @@
+// const btnFiltrarLivrosDeFront = document.getElementById('btnFiltrarLivrosFront');
+
+// btnFiltrarLivrosDeFront.addEventListener("click", filtrarLivros);
+
+const botoes = document.querySelectorAll(".btn");
+
+botoes.forEach((btn) => btn.addEventListener("click", filtrarLivros));
+
+function filtrarLivros() {
+  const elementoBtn = document.getElementById(this.id);
+  const categoria = elementoBtn.value;
+  let livrosFiltrados = livros.filter((livro) => {
+    return livro.categoria == categoria;
+  });
+  console.table(livrosFiltrados);
+}
