0f31753fd5d4e85c6b666f2b5aceaa49bbf0492e
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-11-13 12:29:18 -0300

    commit 6

--- js/metodoFilter.js
+++ js/metodoFilter.js
@@ -10,10 +10,31 @@ function filtrarLivros() {
   const elementoBtn = document.getElementById(this.id);
   const categoria = elementoBtn.value;
   let livrosFiltrados = (categoria == "disponivel")
-    ? livros.filter((livro) => livro.quantidade > 0)
-    : livros.filter((livro) => livro.categoria == categoria);
+    ? filtrarPorDisponibilidade()
+    : filtrarPorCategoria(categoria);
   // console.table(livrosFiltrados);
   exibirOsLivrosNaTela(livrosFiltrados)
+  if (categoria == "disponivel") {
+    const valorTotal = calcularValorTotalDeLivrosDisponiveis(livrosFiltrados);
+    exibirValorTotalDosLivrosDisponiveisNaTela(valorTotal);
+  }
+}
+
+function filtrarPorCategoria(categoria) {
+  return livros.filter((livro) => livro.categoria == categoria);
+}
+
+function filtrarPorDisponibilidade() {
+  return livros.filter((livro) => livro.quantidade > 0);
+}
+
+function exibirValorTotalDosLivrosDisponiveisNaTela(valorTotal) {
+  elementoComValorTotalDeLivrosDisponveis.innerHTML = `
+    <div class="livros__disponiveis">
+      <p>Todos os livros disponíveis por R$ <span id="valor">${valorTotal}</span></p>
+    </div>
+  `;
+
 }
 
 /*
--- js/metodoForEach.js
+++ js/metodoForEach.js
@@ -1,6 +1,8 @@
 const elementoParaInserirLivros = document.getElementById("livros");
+const elementoComValorTotalDeLivrosDisponveis = document.getElementById('valor_total_livros_disponiveis')
 
 function exibirOsLivrosNaTela(listaDeLivros) {
+  elementoComValorTotalDeLivrosDisponveis.innerHTML = "";
   elementoParaInserirLivros.innerHTML = "";
   listaDeLivros
     .forEach((livro) => {
